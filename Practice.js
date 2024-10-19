b5cf7ecb9a0e9f0a664dc09dd5c80dcceff49b38
Salman <59947811+elated-salman@users.noreply.github.com>
2024-10-19 10:03:33 +0530

    Add files via upload

---
+++ Practice.js
@@ -0,0 +1,22 @@
+let a = "Salman"
+let b = 6;
+console.log( a + b )
+
+console.log (typeof(a+b))
+
+
+const a1 = {
+    name : "Salman",
+    section : 1,
+    isPrincipal : false
+}
+a1['domain'] = "IT"
+a1['domain2'] = "comp"
+console.log(a1)
+
+const dict = {
+    salman : "healthy kind ",
+    GBY : "ideal",
+    yakka : "hardwork"
+}
+console.log(dict['yakka'])
